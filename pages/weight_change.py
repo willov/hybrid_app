303b12b736eeeb0d44c44a5edbf8abf768f9c77e
herrTilda <tilda.herrgardh@gmail.com>
2024-05-24 16:26:54 +0200

    trying to add key when Linit and Finit not set by user

--- pages/weight_change.py
+++ pages/weight_change.py
@@ -103,7 +103,8 @@ if 'Linit' not in st.session_state:
 
 
 anthropometrics = {"weight": st.session_state['weight'], "height": st.session_state['height'], 
-                   "Finit": st.session_state['Finit'], "Linit": st.session_state['Linit']
+                   "Finit": st.session_state['Finit'], "Linit": st.session_state['Linit'],
+                   "Ginit": st.session_state['Ginit'], "ECFinit": st.session_state['ECFinit']
                    }
 
 # anthropometrics["sex"] = float(anthropometrics["sex"].lower() in ["male", "man", "men", "boy", "1", "chap", "guy"]) #Converts to a numerical representation
