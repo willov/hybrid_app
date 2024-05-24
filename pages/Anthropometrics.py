2178ea90810777f1b3ab15a1526933d902a9c394
herrTilda <tilda.herrgardh@gmail.com>
2024-05-24 13:57:20 +0200

    removing ability to set Linit and Finti2

--- pages/Anthropometrics.py
+++ pages/Anthropometrics.py
@@ -21,33 +21,35 @@ if 'height' not in st.session_state:
     st.session_state['height'] = 1.85
 if 'age' not in st.session_state:
     st.session_state['age'] = 40.0
-st.session_state['Ginit'] = (1.0 + 2.7)*0.5
-st.session_state['ECFinit'] = 0.7*0.235*st.session_state['weight']  
-if 'Finit' not in st.session_state:
-    if st.session_state['sex']== 'Woman':
-        st.session_state['Finit'] = (st.session_state['weight']/100.0)*(0.14*st.session_state['age'] + 39.96*math.log(st.session_state['weight']/((0.01*st.session_state['height'])**2.0)) - 102.01)
-    elif st.session_state['sex']== 'Man': 
-        st.session_state['Finit'] = (st.session_state['weight']/100.0)*(0.14*st.session_state['age'] + 37.31*math.log(st.session_state['weight']/((0.01*st.session_state['height'])**2.0)) - 103.95) 
-if 'Linit' not in st.session_state:
-    st.session_state['Linit'] = st.session_state['weight'] - (st.session_state['Finit'] + (1.0 + 2.7)*st.session_state['Ginit'] + st.session_state['ECFinit'])
+# st.session_state['Ginit'] = (1.0 + 2.7)*0.5
+# st.session_state['ECFinit'] = 0.7*0.235*st.session_state['weight']  
+# if 'Finit' not in st.session_state:
+#    if st.session_state['sex']== 'Woman':
+#        st.session_state['Finit'] = (st.session_state['weight']/100.0)*(0.14*st.session_state['age'] + 39.96*math.log(st.session_state['weight']/((0.01*st.session_state['height'])**2.0)) - 102.01)
+#    elif st.session_state['sex']== 'Man': 
+#        st.session_state['Finit'] = (st.session_state['weight']/100.0)*(0.14*st.session_state['age'] + 37.31*math.log(st.session_state['weight']/((0.01*st.session_state['height'])**2.0)) - 103.95) 
+# if 'Linit' not in st.session_state:
+#    st.session_state['Linit'] = st.session_state['weight'] - (st.session_state['Finit'] + (1.0 + 2.7)*st.session_state['Ginit'] + st.session_state['ECFinit'])
 
 
-anthropometrics = {"weight": st.session_state['weight'], "ECFinit": st.session_state['ECFinit'],
-                   "height": st.session_state['height'], "Linit": st.session_state['Linit'],
-                   "Finit": st.session_state['Finit'], "Ginit": st.session_state['Ginit']}
+anthropometrics = {"weight": st.session_state['weight'], "height": st.session_state['height'], 
+                   "sex": st.session_state['sex']
+                   }
+# "ECFinit": st.session_state['ECFinit'], "Finit": st.session_state['Finit'], "Ginit": st.session_state['Ginit'], "Linit": st.session_state['Linit'],
+                   
 
 anthropometrics["sex"] = st.selectbox("Sex:", ["Man", "Woman"], ["Man", "Woman"].index(st.session_state['sex']), key="sex")
 anthropometrics["weight"] = st.number_input("Weight (kg):", 0.0, 1000.0, st.session_state['weight'], key="weight") # max, min 
 anthropometrics["age"] = st.number_input("Age (years):", 0.0, 100.0, st.session_state['age'], key="age") # max, min 
 anthropometrics["height"] = st.number_input("Height (m):", 0.0, 2.5, st.session_state['height'],  key="height") # st.session_state['height'], 0.1, 
 
-fat_known = st.checkbox("Do you know your fat mass?")
-if fat_known:
-    anthropometrics["Finit"] = st.number_input("Fat mass (kg):", 0.0, 1000.0, st.session_state.Finit, 0.1, key="Finit")
+# fat_known = st.checkbox("Do you know your fat mass?")
+# if fat_known:
+#     anthropometrics["Finit"] = st.number_input("Fat mass (kg):", 0.0, 1000.0, st.session_state.Finit, 0.1, key="Finit")
 
-lean_known = st.checkbox("Do you know your lean mass?")
+# lean_known = st.checkbox("Do you know your lean mass?")
 
-if lean_known:
-    anthropometrics["Linit"] = st.number_input("Lean mass (kg):", 0.0, 1000.0, st.session_state.Linit, 0.1, key="Linit")
-    anthropometrics['Ginit'] = anthropometrics['weight'] - (anthropometrics['Finit'] + anthropometrics['Linit'] + anthropometrics['ECFinit'])
+# if lean_known:
+#     anthropometrics["Linit"] = st.number_input("Lean mass (kg):", 0.0, 1000.0, st.session_state.Linit, 0.1, key="Linit")
+#    anthropometrics['Ginit'] = anthropometrics['weight'] - (anthropometrics['Finit'] + anthropometrics['Linit'] + anthropometrics['ECFinit'])
 
