46fd44c864053d9fe579d1847b6dc91b16afee8b
herrTilda <tilda.herrgardh@gmail.com>
2024-06-05 19:53:02 +0200

    adding meal kcal to plot legend and max DBP = SBP

--- pages/1_weight_change.py
+++ pages/1_weight_change.py
@@ -122,7 +122,7 @@ if 'height' not in st.session_state:
     st.session_state['height'] = 1.85
 if 'age' not in st.session_state:
     st.session_state['age'] = 40.0
-st.session_state['Ginit'] = (1.0 + 2.7)*0.5
+st.session_state['Ginit'] = 0.5
 st.session_state['ECFinit'] = 0.7*0.235*st.session_state['weight']  
 if 'Finit' not in st.session_state:
     if st.session_state['sex']== 'Woman':
