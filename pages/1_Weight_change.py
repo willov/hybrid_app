97e033b252f83db1da20a7a9f5b5bf3370871785
Henrik Podéus Derelöv <henrik.podeus.derelov@liu.se>
2025-11-10 11:56:33 +0100

    Remove unused imports for improved code clarity in weight and blood pressure change simulations

--- pages/1_Weight_change.py
+++ pages/1_Weight_change.py
@@ -1,13 +1,9 @@
 import os
-import json
 import pandas as pd
 import numpy as np
 import streamlit as st
 import math 
 import altair as alt
-from array import array
-import plotly.express as px
-import plotly.graph_objects as go
 import copy
 
 # Install sund in a custom location
